19cc488021b8604b3441571e999a037a1afdbfce
RSaivarsha <saivarsha1605@gmail.com>
2025-02-26 10:19:24 -0500

    Completed database-setup

--- tests/factories.py
+++ tests/factories.py
@@ -3,18 +3,20 @@ Test Factory to make fake objects for testing
 """
 
 import factory
-from service.models import YourResourceModel
+from service.models import Inventory as InventoryModel
 
 
-class YourResourceModelFactory(factory.Factory):
+class InventoryModelFactory(factory.Factory):
     """Creates fake pets that you don't have to feed"""
 
     class Meta:  # pylint: disable=too-few-public-methods
         """Maps factory to data model"""
 
-        model = YourResourceModel
-
-    id = factory.Sequence(lambda n: n)
-    name = factory.Faker("first_name")
+        model = InventoryModel
+    name = factory.Faker("word")
+    product_id = factory.Sequence(lambda n: 1000 + n)
+    quantity = 10
+    condition = "New"
+    restock_level = 5
 
     # Todo: Add your other attributes here...
